998664dbaf7aee2b75b50da7901abea9e17e9124
Martin Ben <mebenav88@gmail.com>
2015-12-22 01:17:58 -0600

    add blog/title to routes

--- src/app.js
+++ src/app.js
@@ -11,8 +11,10 @@ app.get('/', function(req, res){
   res.send("<h1>I am in love with treehouse</h1>")
 });
 
-app.get('/blog', function(req, res){
-  res.send(posts);
+app.get('/blog/:title', function(req, res){
+  var title = req.params.title;
+  var post = posts[title];
+  res.send(post);
 });
 app.listen(3000, function(){
   console.log("The frontend server is running on port 3000.")
